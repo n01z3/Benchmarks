d826d0fbc76aa271c1934a1db2f7b1ed6bc0d86e
Evgeny Nizhibitsky <nizhibitsky@ya.ru>
2017-04-14 13:11:22 +0300

    Use python bin variable. Swap 2gpu/4gpu scripts

--- bench_4gpu.sh
+++ bench_4gpu.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-~/anaconda3/bin/python bench.py --n 1 --net vgg19
-~/anaconda3/bin/python bench.py --n 2 --net vgg19
-~/anaconda3/bin/python bench.py --n 1 --net Inception-BN
-~/anaconda3/bin/python bench.py --n 2 --net Inception-BN
-~/anaconda3/bin/python bench.py --n 1 --net resnet-152
-~/anaconda3/bin/python bench.py --n 2 --net resnet-152
-~/anaconda3/bin/python bench.py --n 1 --net resnext-101
-~/anaconda3/bin/python bench.py --n 2 --net resnext-101
+
+PYTHON_BIN=$HOME/anaconda3/bin/python
+
+$PYTHON_BIN bench.py --n 1 --net vgg19
+$PYTHON_BIN bench.py --n 2 --net vgg19
+$PYTHON_BIN bench.py --n 3 --net vgg19
+$PYTHON_BIN bench.py --n 4 --net vgg19
+$PYTHON_BIN bench.py --n 1 --net Inception-BN
+$PYTHON_BIN bench.py --n 2 --net Inception-BN
+$PYTHON_BIN bench.py --n 3 --net Inception-BN
+$PYTHON_BIN bench.py --n 4 --net Inception-BN
+$PYTHON_BIN bench.py --n 1 --net resnet-152
+$PYTHON_BIN bench.py --n 2 --net resnet-152
+$PYTHON_BIN bench.py --n 3 --net resnet-152
+$PYTHON_BIN bench.py --n 4 --net resnet-152
+$PYTHON_BIN bench.py --n 1 --net resnext-101
+$PYTHON_BIN bench.py --n 2 --net resnext-101
+$PYTHON_BIN bench.py --n 3 --net resnext-101
+$PYTHON_BIN bench.py --n 4 --net resnext-101
